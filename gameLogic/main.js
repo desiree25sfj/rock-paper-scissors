af98cd9b94358a21a8ceef7fbadac1c113479368
desiree25sfj <desiree25sfj@gmail.com>
2025-09-18 15:58:01 +0200

    added battleAnimation

--- gameLogic/main.js
+++ gameLogic/main.js
@@ -16,47 +16,47 @@ const rockButton = document.querySelector("#rock");
 const paperButton = document.querySelector("#paper");
 const scissorsButton = document.querySelector("#scissors");
 
-// function playBattleAnimation(playerChoice, computerChoice, callback) {
-//   const battle = document.querySelector(".battle");
-//   const playerHand = document.querySelector(".playerHand");
-//   const computerHand = document.querySelector(".computerHand");
-//   const choices = document.querySelector(".choices");
+function playBattleAnimation(playerChoice, computerChoice, callback) {
+  const battle = document.querySelector(".battle");
+  const playerHand = document.querySelector(".playerHand");
+  const computerHand = document.querySelector(".computerHand");
+  const choices = document.querySelector(".gameButtons");
 
-//   choices.style.display = "none";
+  choices.style.display = "none";
 
-//   battle.classList.add("show");
+  battle.classList.add("show");
 
-//   playerHand.textContent = "✊";
-//   computerHand.textContent = "✊";
+  playerHand.textContent = "✊";
+  computerHand.textContent = "✊";
 
-//   let count = 0;
-//   const interval = setInterval(() => {
-//     count++;
-//     playerHand.style.transform = "scale(1.2)";
-//     computerHand.style.transform = "scale(1.2)";
+  let count = 0;
+  const interval = setInterval(() => {
+    count++;
+    playerHand.style.transform = "scale(1.2)";
+    computerHand.style.transform = "scale(1.2)";
 
-//     setTimeout(() => {
-//       playerHand.style.transform = "scale(1)";
-//       computerHand.style.transform = "scale(1)";
-//     }, 150);
+    setTimeout(() => {
+      playerHand.style.transform = "scale(1)";
+      computerHand.style.transform = "scale(1)";
+    }, 100);
 
-//     if (count === 3) {
-//       clearInterval(interval);
+    if (count === 3) {
+      clearInterval(interval);
 
-//       const emojis = {rock: "✊", paper: "✋", scissors: "✌️" };
-//       playerHand.textContent = emojis[playerChoice];
-//       computerHand.textContent = emojis[computerChoice];
+      const emojis = {rock: "✊", paper: "✋", scissors: "✌️" };
+      playerHand.textContent = emojis[playerChoice];
+      computerHand.textContent = emojis[computerChoice];
 
-//       setTimeout(() => {
-//         battle.classList.remove("show");
+      setTimeout(() => {
+        battle.classList.remove("show");
 
-//         choices.style.display ="flex";
+        choices.style.display ="flex";
 
-//         callback();
-//       }, 1200);
-//     }
-//   }, 600);
-// }
+        callback();
+      }, 650);
+    }
+  }, 400);
+}
 
 function congratulateWinner() {
   resultCircle.classList.remove("win", "lose", "tie");
